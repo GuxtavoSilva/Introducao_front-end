3a2bacb820a309fe31cd864d81c4802cfa4f3b0f
GuxtavoSilva <gustavo.silva36152@gmail.com>
2024-06-07 14:59:15 -0300

    Introdução a front-end

---
+++ 8-DOM/script.js
@@ -0,0 +1,7 @@
+var titulo = document.querySelector('h1')
+titulo.style.color = 'red';
+console.log(titulo);
+
+var button = document.querySelector('button')
+
+button.classList.add('btn')
---
+++ 8-desafio14/script.js
@@ -0,0 +1,3 @@
+document.body.style.backgroundColor = "violet";
+document.querySelector('h1').style.fontSize = '80px';
+document.querySelector('p').style.backgroundColor = 'green'
